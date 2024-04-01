6bc36fc008b9d4f83ded0620866f70af576b64f8
German Vladimir <vladimir.german.and@yandex.ru>
2024-04-01 17:29:21 +0700

    asd

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+
+from .validators import real_age
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', blank=True,
+                              upload_to='birthdays_images',
+                              )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
